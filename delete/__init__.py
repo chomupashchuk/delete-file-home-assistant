e86b7f930bd2a3bdf95a69d6919f4a662e4fc521
chomupashchuk <50545689+chomupashchuk@users.noreply.github.com>
2020-03-07 17:28:27 +0200

    Add files via upload

--- delete/__init__.py
+++ delete/__init__.py
@@ -1,9 +1,15 @@
 import os
+import time
 import logging
 
 ATTR_FILE = "file"
-SERVICE = "file"
+ATTR_FOLDER = "folder"
+ATTR_TIME = "time"
+SERVICE_FILE = "file"
+SERVICE_FOLDER = "files_in_folder"
 DEFAULT_FILE = ""
+DEFAULT_FOLDER = ""
+DEFAULT_TIME = 3600
 DOMAIN = "delete"
 
 _LOGGER = logging.getLogger(__name__)
@@ -19,8 +25,27 @@ def setup(hass, config):
             os.remove(file_path)
             _LOGGER.warning("Deleted {}".format(file_path))
         else:
-            _LOGGER.info("{} is not a file".format(file_path))         
-    
-    hass.services.register(DOMAIN, SERVICE, handle_delete_file)
+            _LOGGER.info("{} is not a file".format(file_path))
+            raise
+
+    def handle_delete_files_in_folder(call):
+        """Handle the service call."""
+        folder_path = call.data.get(ATTR_FOLDER, DEFAULT_FOLDER)
+        folder_time = call.data.get(ATTR_TIME, DEFAULT_TIME)
+        now = time.time()
+        
+        if os.path.isdir(folder_path):
+            for file in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, file)
+                if os.stat(file_path).st_mtime < now - folder_time:
+                    if os.path.isfile(file_path):
+                        _LOGGER.warning("Deleted {}".format(file_path))
+                        os.remove(file_path)
+        else:
+            _LOGGER.info("{} is not a folder".format(folder_path))
+            raise
+        
+    hass.services.register(DOMAIN, SERVICE_FILE, handle_delete_file)
+    hass.services.register(DOMAIN, SERVICE_FOLDER, handle_delete_files_in_folder)
     # Return boolean to indicate that initialization was successfully.
     return True
